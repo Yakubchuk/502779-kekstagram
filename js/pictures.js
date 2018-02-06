ff79a78874beaa6c4ebb9cd007504246291476cf
Yakubchuk <imy@inbox.ru>
2018-02-06 22:51:28 +0300

    mixin the massive variant #2

--- js/pictures.js
+++ js/pictures.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-spacing */
 'use strict';
 var pictureTemplate = document.querySelector('#picture-template').content;
 var picturesList = document.querySelector('.pictures');
@@ -16,17 +17,33 @@ function getRandomNum(max, min) {
   return Math.floor(Math.random() * (max + 1 - min));
 }
 
-// ---функция выбора одного\двух комментариев
-function getRndQuantity() {
-  var num = [];
-  var obj = COMMENTS;
-  var count = getRandomNum(2, 1) + 1;
-  // num.slice(0, num.length);
-  for (var j = 0; j < count; j++) {
-    num[j] = obj[Math.floor(Math.random() * obj.length)];
-  }
-  return num;
+// ---перемешиваем массив
+function compareRandom(a, b) {
+  return Math.random() - 0.5;
 }
+
+// ---функция выбора одного\двух комментариев
+function mixinDatas() {
+  var currentComments = [];
+  var block = COMMENTS.sort(compareRandom);
+  var counter = Math.round(Math.random()+1);
+  for (var n = 0; n < counter; n++){
+    currentComments[n] = block[n];
+  }
+  return currentComments;
+}
+// mixinDatas();
+
+// function getRndQuantity() {
+//   var num = [];
+//   var obj = COMMENTS;
+//   var count = getRandomNum(2, 1) + 1;
+//   // num.slice(0, num.length);
+//   for (var j = 0; j < count; j++) {
+//     num[j] = obj[Math.floor(Math.random() * obj.length)];
+//   }
+//   return num;
+// }
 
 // ---Запонлнение Массива Объектов Данными
 var photos = [];
@@ -35,7 +52,7 @@ for (var i = 0; i < 25; i++) {
     {
       urls: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomNum(200, 15),
-      comments: getRndQuantity()
+      comments: mixinDatas()
     };
 }
 
@@ -56,7 +73,7 @@ for (var i = 0; i < photos.length; i++) {
 picturesList.appendChild(fragment);
 
 // --- Заполняем ДОМ данными из первого объекта массива
-mainPicture.classList.remove('hidden');
+// mainPicture.classList.remove('hidden');
 mainPicture.querySelector('.gallery-overlay-image').src = photos[0].urls;
 mainPicture.querySelector('.likes-count').textContent = photos[0].likes;
 mainPicture.querySelector('.comments-count').textContent = photos[0].comments.length;
