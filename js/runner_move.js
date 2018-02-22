20e8f6b145710d23801dafbd38b66be24d10a53f
Yakubchuk <imy@inbox.ru>
2018-02-22 20:21:38 +0300

    slider

--- js/runner_move.js
+++ js/runner_move.js
@@ -3,17 +3,22 @@
   window.RUNNER.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
     var startCoords = {x: evt.clientX};
+    var maxWidth = 460;
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
       var shift = {x: startCoords.x - moveEvt.clientX};
       startCoords = {
         x: moveEvt.clientX
       };
-      window.RUNNER.style.left = (window.RUNNER.offsetLeft - shift.x) + 'px';
-      if (window.RUNNER.offsetLeft - shift.x < 0 || window.RUNNER.offsetLeft - shift.x > 460) {
-        document.removeEventListener('mousedown', onMouseUp);
-        document.removeEventListener('mousemove', onMouseMove);
+      var leftBreak = document.querySelector('.upload-effect-level').getBoundingClientRect();
+      var curMove = (window.RUNNER.offsetLeft - shift.x);
+      if (moveEvt.clientX <= leftBreak.x) {
+        curMove = 0;
       }
+      if (moveEvt.clientX >= leftBreak.x + maxWidth) {
+        curMove = maxWidth;
+      }
+      window.RUNNER.style.left = curMove + 'px';
       window.onRunnerShift();
     };
     var onMouseUp = function (upEvt) {
