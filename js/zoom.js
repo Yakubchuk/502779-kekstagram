9de6db0182c70cbe03a55bac59a0d1e79cb43a74
Yakubchuk <imy@inbox.ru>
2018-02-26 20:00:54 +0300

    clear popup.js and fix eventlisteners in zoom.js

--- js/zoom.js
+++ js/zoom.js
@@ -1,13 +1,13 @@
 'use strict';
 // //////////////////////////// ---------------------------- Маштаб изображения -------------------- ///////////////////////////
 (function () {
-  var buttonDec = document.querySelector('.upload-resize-controls-button-dec');
-  var buttonInc = document.querySelector('.upload-resize-controls-button-inc');
+  window.buttonDec = document.querySelector('.upload-resize-controls-button-dec');
+  window.buttonInc = document.querySelector('.upload-resize-controls-button-inc');
   var sizeValue = document.querySelector('.upload-resize-controls-value');
   var countZoom = 25;
 
   // --- Маштаб Плюс
-  var onSizeIncClick = function () {
+  window.onSizeIncClick = function () {
     var currentCount = parseInt(sizeValue.value, 0);
     if (currentCount - countZoom >= countZoom) {
       sizeValue.value = currentCount - countZoom + '%';
@@ -15,19 +15,12 @@
     }
   };
   // --- Маштаб Минус
-  var onSizeDecClick = function () {
+  window.onSizeDecClick = function () {
     var currentCount = parseInt(sizeValue.value, 0);
     if (currentCount + countZoom <= 100) {
       sizeValue.value = currentCount + countZoom + '%';
       window.IMG_PREV.style.transform = 'scale(' + (currentCount + countZoom) / 100 + ')';
     }
   };
-
-  buttonDec.addEventListener('click', function () {
-    onSizeIncClick();
-  });
-  buttonInc.addEventListener('click', function () {
-    onSizeDecClick();
-  });
 
 })();
